804bbd6aea83784e16cdca40551f7765fd8e5b9f
abhi-pandey-ai <abhishekk59608@gmail.com>
2025-10-10 11:27:37 +0530

    fix some error edit or delete button

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "all-india";
+$database = "demo";
 
 $conn = mysqli_connect($servername,$username,$password,$database);
 if(!$conn){
